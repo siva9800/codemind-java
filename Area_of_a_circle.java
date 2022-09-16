0f41358d577e768ce165c217a00df06327732e7c
SIVA POTHULA <106661571+siva9800@users.noreply.github.com>
2022-09-16 12:24:19 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float x;         x=sc.nextInt();         System.out.format("%.2f",3.14*(x*x));     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float x;
+        x=sc.nextInt();
+        System.out.format("%.2f",3.14*(x*x));
+    }
+}
